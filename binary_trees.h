dccfaaf480cd76acccea9807fec32841c768bfb8
TheOrbMaverick <enangita@gmail.com>
2024-02-26 17:08:02 +0100

    checking the print

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,8 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -28,6 +31,9 @@ typedef struct binary_tree_s avl_t;
 
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
+
+/* Binary tree print */
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 

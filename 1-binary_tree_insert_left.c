1998a7372dd5df289d1503ca662fed49bf055be8
TheOrbMaverick <enangita@gmail.com>
2024-02-27 16:14:42 +0100

    1 betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,26 +10,26 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    /* Check if the parent is NULL */
-    if (parent == NULL)
-    {
-        return NULL;
-    }
+	/* Check if the parent is NULL */
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    /* Create a new node */
-    binary_tree_t *new_node = binary_tree_node(parent, value);
+	/* Create a new node */
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-    /* If the parent already has a left child */
-    if (parent->left != NULL)
-    {
-        /* Set the left child of the new node to the current left child of the parent */
-        new_node->left = parent->left;
-        /* Update the parent pointer of the current left child */
-        new_node->left->parent = new_node;
-    }
+	/* If the parent already has a left child */
+	if (parent->left != NULL)
+	{
+		/* Set the left child of the new node to the current left child of the parent */
+		new_node->left = parent->left;
+		/* Update the parent pointer of the current left child */
+		new_node->left->parent = new_node;
+	}
 
-    /* Set the left child of the parent to the new node */
-    parent->left = new_node;
-    /* Return a pointer to the created node */
-    return new_node;
+	/* Set the left child of the parent to the new node */
+	parent->left = new_node;
+	/* Return a pointer to the created node */
+	return (new_node);
 }

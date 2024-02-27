118ee0586999a74653ec561b168e7ef87f3a834f
TheOrbMaverick <enangita@gmail.com>
2024-02-27 23:41:21 +0100

    choo choo

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,12 +10,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
 
-	if (tree == NULL)
+	/* If tree is NULL or has no children, return 0 */
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
+	/* Recursively compute the height of the left subtree */
 	height_left = binary_tree_height(tree->left);
+
+	/* Recursively compute the height of the right subtree */
 	height_right = binary_tree_height(tree->right);
 
+	/** Return the maximum height of the left and right subtrees, plus 1 */
 	return ((height_left > height_right ? height_left : height_right) + 1);
 }
 

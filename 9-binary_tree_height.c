375d5e991b4f4d1f614d85270bfb01d286c2f55c
TheOrbMaverick <enangita@gmail.com>
2024-02-27 23:04:37 +0100

    working?

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,8 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
 
-	/* If tree is NULL, return 0 */
-	if (tree == NULL)
+	/* If tree is NULL or has no children, return 0 */
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	/* Recursively compute the height of the left subtree */
@@ -20,6 +20,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/* Recursively compute the height of the right subtree */
 	height_right = binary_tree_height(tree->right);
 
-	/** Return the maximum height of the left and right subtrees */
+	/** Return the maximum height of the left and right subtrees, plus 1 */
 	return ((height_left > height_right ? height_left : height_right) + 1);
 }

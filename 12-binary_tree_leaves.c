a19a8ee50e63829621c82b04b68054a84ce053d3
TheOrbMaverick <enangita@gmail.com>
2024-02-27 23:09:15 +0100

    checking

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,25 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures the size of a binary tree
+ * binary_tree_leaves - Counts the leaves in a binary tree
  * @tree: Pointer to the root node of the tree
  *
- * Return: Size of the tree, or 0 if tree is NULL
+ * Return: Number of leaves in the tree, or 0 if tree is NULL
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t size_left, size_right;
-
-	/* If tree is NULL, return 0 */
 	if (tree == NULL)
 		return (0);
 
-	/* Recursively compute the size of the left subtree */
-	size_left = binary_tree_size(tree->left);
+	/* If tree has no children, it's a leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	/* Recursively compute the size of the right subtree */
-	size_right = binary_tree_size(tree->right);
-
-	/* Return the sum of the sizes of the left and right subtrees plus 1 */
-	return (size_left + size_right + 1);
+	/* Recursively count the leaves in the left and right subtrees */
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }

47473f944c560e53d4e67125048a381f8b0400be
TheOrbMaverick <enangita@gmail.com>
2024-02-28 21:29:06 +0100

    going

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: 1 if the tree is full, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	/* If tree is NULL, it's full by definition */
+	if (tree == NULL)
+		return (1);
+
+	/* If the tree has no children, it's full */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* If the tree has both children, check recursively */
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	/* If the tree has only one child, it's not full */
+	return (0);
+}
+
+/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree
  *
